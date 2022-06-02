e2288312dc04c0a1b556cafd2f1860811c054e03
JirufKa20 <106743206+JirufKa20@users.noreply.github.com>
2022-06-02 13:49:08 +0200

    Update main.py
    Popis

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ cislo1 = int(input("Zadejte cislo1: "))
 cislo2 = int(input("Zadejte cislo2: "))
 vysledek = cislo1+cislo2
 print(vysledek)
+cislo1 = int(input("Zadejte cislo1: "))
+cislo2 = int(input("Zadejte cislo2: "))
+vysledek = cislo1-cislo2
+print(vysledek)
