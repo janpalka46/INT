d31f116b4d45bb72bac037423a2562fe76afd633
janpalka46 <janpalka46@gmail.com>
2022-06-09 12:42:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-cislo1 = int(input("Zadejte cislo1: "))
-cislo2 = int(input("Zadejte cislo2: "))
-vysledek = cislo1+cislo2
-print(vysledek)
-cislo1 = int(input("Zadejte cislo1: "))
-cislo2 = int(input("Zadejte cislo2: "))
-vysledek = cislo1-cislo2
-print(vysledek)
+print("Kalkulacka \n")
+vyraz = input("Zadejte vyraz: ")
