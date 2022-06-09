10c4cbc42c0414aaab4c10058aa364879aa75236
janpalka46 <janpalka46@gmail.com>
2022-06-09 13:29:45 +0200

    Čtverec

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-for i in range(10):
-    print(i)
+
+from ast import While
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+ 
+window = pygame.display.set_mode((600, 600))
+ 
+window.fill((255, 255, 255))
+ 
+pygame.draw.rect(window, (0, 0, 255), [100, 100, 400, 100], 2)
+while True:
+    pygame.display.update()
