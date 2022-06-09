9501b59eaafac8b9e051febaa2ecf6be76cd1602
janpalka46 <janpalka46@gmail.com>
2022-06-09 13:06:51 +0200

    Kamilova verze

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
-
-
 print ("Zdravičko")
 
-
-
-=======
-print("Kalkulacka \n")
-vyraz = input("Zadejte vyraz: ")
->>>>>>> 07be9610fa3dd1c7c6e3863bf87684bae77c0a67
-print("Ahoj svete")
