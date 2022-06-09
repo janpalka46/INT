63191bca2e601d9958b62bda0e97402c7cada259
janpalka46 <janpalka46@gmail.com>
2022-06-09 13:37:36 +0200

    Příklad

---
+++ logo.py
@@ -0,0 +1,4 @@
+def VypsatLogo():
+    print("###########")
+    print("#         #")
+    print("###########")
--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-
-from ast import While
-import pygame
-from pygame.locals import *
- 
-pygame.init()
- 
-window = pygame.display.set_mode((600, 600))
- 
-window.fill((255, 255, 255))
- 
-pygame.draw.rect(window, (0, 0, 255), [100, 100, 400, 100], 2)
-while True:
-    pygame.display.update()
+from logo.py import VypsatLogo
+print("Kalkulačka")
+VypsatLogo()
