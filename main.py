095744754ede19a92010565ce8784bcd45e021b8
janpalka46 <janpalka46@gmail.com>
2022-06-09 12:52:56 +0200

    Vrácení verze

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def Valide (vyraz):
     return False
 
@@ -7,3 +8,13 @@ print (Valide(vyraz))
 
 
 
+=======
+cislo1 = int(input("Zadejte cislo1: "))
+cislo2 = int(input("Zadejte cislo2: "))
+vysledek = cislo1+cislo2
+print(vysledek)
+cislo1 = int(input("Zadejte cislo1: "))
+cislo2 = int(input("Zadejte cislo2: "))
+vysledek = cislo1-cislo2
+print(vysledek)
+>>>>>>> parent of d31f116 (Update main.py)
