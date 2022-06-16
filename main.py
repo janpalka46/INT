b1985b3e0e577d5e05b80137750918a4b367fee8
janpalka46 <janpalka46@gmail.com>
2022-06-16 13:44:20 +0200

    init

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from logo import VypsatLogo
-from Vyraz import Vyraz
-print("Kalkulačka")
-VypsatLogo()
+from tkinter import Widget
+import PyQt5
+from PyQt5.QtWidgets import *
+import sys
 
-i=input ("Zadej číslo")
+def main():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.resize(800,800)
+    window.setWindowTitle("Test")
+    window.show()
+    while True:
+        continue
+
+main()
